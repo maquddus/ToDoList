f2efc6cb9c11bf5c57fa2f359d4397e4bec0d4b5
aquddus@me.com <aquddus@me.com>
2015-08-07 21:31:21 +0100

    added user association

--- db/schema.rb
+++ db/schema.rb
@@ -11,12 +11,13 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150804220753) do
+ActiveRecord::Schema.define(version: 20150805223733) do
 
   create_table "tasks", force: :cascade do |t|
     t.text     "content"
     t.datetime "created_at", null: false
     t.datetime "updated_at", null: false
+    t.integer  "user_id"
   end
 
   create_table "users", force: :cascade do |t|
