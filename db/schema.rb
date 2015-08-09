26cdf646121fec86d23072a85766bf863e61edb6
aquddus@me.com <aquddus@me.com>
2015-08-09 22:40:00 +0100

    final completed version 1.0

--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,7 @@
 #
 # It's strongly recommended that you check this file into your version control system.
 
-ActiveRecord::Schema.define(version: 20150807203551) do
+ActiveRecord::Schema.define(version: 20150809212549) do
 
 # Could not dump table "tasks" because of following NoMethodError
 #   undefined method `[]' for nil:NilClass
